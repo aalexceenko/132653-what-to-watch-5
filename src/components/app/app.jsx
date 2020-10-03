7268ab409867c7dba885cbeca5d70ec7fe02cff9
aalexceenko <xnyxnymrik@bk.ru>
2020-10-03 22:46:00 +0300

    add components other screen

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import GeneralPage from "../general-page/general-page";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import AddReviewScreen from "../add-review-screen/add-review-screen";
+import FilmScreen from "../film-screen/film-screen";
+import LoginScreen from "../login-screen/login-screen";
+import MyListScreen from "../my-list-screen/my-list-screen";
+import PlayerScreen from "../player-screen/player-screen";
 
 
 const App = ({title, genre, releaseDate}) => {
 
   return (
-    <GeneralPage title={title} genre={genre} releaseDate={releaseDate} />
+  // <GeneralPage title={title} genre={genre} releaseDate={releaseDate} />
+
+
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <GeneralPage title={title} genre={genre} releaseDate={releaseDate} />
+        </Route>
+        <Route exact path="/login">
+          <LoginScreen />
+        </Route>
+        <Route exact path="/mylist">
+          <MyListScreen />
+        </Route>
+        <Route exact path="/films/:id">
+          <FilmScreen />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReviewScreen />
+        </Route>
+        <Route exact path="/player/:id">
+          <PlayerScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
